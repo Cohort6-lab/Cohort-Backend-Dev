522693f85c1f0bd064b9bf2333c493e3c17021e5
Dumka-code <preciousdpb2000@gmail.com>
2023-04-30 00:05:10 +0100

    README.md

--- Web_server/server.php
+++ Web_server/server.php
@@ -3,6 +3,6 @@
 
 header('Content-Type: text/html');
 
-echo file_get_contents('../../Cohort-Front-End-Dev/public/index.html');
+echo file_get_contents('../../Cohort-Front-End-Dev/build/index.html');
 
 ?>
